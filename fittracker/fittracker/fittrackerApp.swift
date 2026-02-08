724d0403e997456e709bbb97eb32c978fd287c87
Matej Herman <hermama8@fit.cvut.cz>
2026-02-08 10:39:51 +0100

    initial push

---
+++ fittracker/fittracker/fittrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  fittrackerApp.swift
+//  fittracker
+//
+//  Created by Matěj Heřman on 08.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct fittrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fittracker/fittrackerTests/fittrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  fittrackerTests.swift
+//  fittrackerTests
+//
+//  Created by Matěj Heřman on 08.02.2026.
+//
+
+import Testing
+@testable import fittracker
+
+struct fittrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
